e97987b7f572886f2be18c66a586857052f15fce
Haiyang-coder <shk4176@163.com>
2024-03-04 21:14:16 +0800

    增加对数据库的封装

---
+++ PlayServer/Public.h
@@ -0,0 +1,49 @@
+#pragma once
+#include"Socket.h"
+#include<string.h>
+
+class Buffer : public std::string
+{
+public:
+	Buffer(size_t size) : std::string() { resize(size); }
+	Buffer(const std::string& str) : std::string(str) {  }
+	Buffer(const char* str) : std::string(str) {  }
+	Buffer(const char* str, size_t length) : std::string(str) {
+		resize(length);
+		memcpy((char*)c_str(), str, length);
+	}
+	Buffer(const char* begin, const char* end) : std::string()
+	{
+		long int len = end - begin;
+		if (len > 0)
+		{
+			resize(len);
+			memcpy((char*)c_str(), begin, len);
+		}
+	}
+	Buffer() : std::string() {}
+	~Buffer() {}
+
+public:
+	//给非常量对象使用
+	operator char* ()
+	{
+		return const_cast<char*>(c_str());
+	}
+	//给常量对象使用
+	operator char* () const
+	{
+		return const_cast<char*>(c_str());
+	}
+	operator const char* ()
+	{
+		return c_str();
+	}
+	operator const char* () const
+	{
+		return c_str();
+	}
+
+private:
+
+};
--- PlayServer/Socket.h
+++ PlayServer/Socket.h
@@ -6,51 +6,7 @@
 #include<arpa/inet.h>
 #include<string>
 #include<fcntl.h>
-class Buffer : public std::string
-{
-public:
-	Buffer(size_t size) : std::string() { resize(size); }
-	Buffer(const std::string& str) : std::string(str) {  }
-	Buffer(const char* str) : std::string(str) {  }
-	Buffer(const char* str, size_t length) : std::string(str) {
-		resize(length);
-		memcpy((char*)c_str(), str, length);
-	}
-	Buffer(const char* begin, const char* end) : std::string()
-	{
-		long int len = end - begin;
-		if (len > 0)
-		{
-			resize(len);
-			memcpy((char*)c_str(), begin, len);
-	}
-	}
-	Buffer() : std::string() {}
-	~Buffer(){}
 
-public:
-	//给非常量对象使用
-	operator char* ()
-	{
-		return const_cast<char*>(c_str());
-	}
-	//给常量对象使用
-	operator char* () const
-	{
-		return const_cast<char*>(c_str());
-	}
-	operator const char* ()
-	{
-		return c_str();
-	}
-	operator const char* () const
-	{
-		return c_str();
-	}
-
-private:
-
-};
 
 enum SOCKATTR {
 	SOCK_ISSERVER = 1,//0是客户端 1是服务器 
